80f9248641639e00fbb0a84cf0ed5ac7f9f467d4
Naflah Shafa <11211066@student.itk.ac.id>
2023-09-29 22:04:09 +0800

    commit 1

---
+++ rumus.js
@@ -0,0 +1,16 @@
+// Local Module
+exports.luasPersegi = function(number1) {
+    return number1 * number1
+}
+
+exports.kelilingPersegi = function(number1) {
+    return number1 * 4
+}
+
+exports.luasPersegiPanjang = function(number1, number2) {
+    return number1 * number2
+}
+
+exports.kelilingPersegiPanjang = function(number1, number2) {
+    return (2 * number1) + (2 * number2)
+}
---
+++ soal1.js
@@ -0,0 +1,7 @@
+// Local Module
+const hitung = require('./rumus.js');
+// Menghitung luas dan keliling dari persegi dan persegi panjang
+console.log(hitung.luasPersegi(5));
+console.log(hitung.kelilingPersegi(7));
+console.log(hitung.luasPersegiPanjang(4, 5));
+console.log(hitung.kelilingPersegiPanjang(8, 4));
